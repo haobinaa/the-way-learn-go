69b9c495842eac85a709c56145828c507a04d944
leobhao <815754194@qq.com>
2023-07-04 14:30:18 +0800

    func option 范式

--- code-block/func_option.go
+++ code-block/func_option.go
@@ -1,12 +1,65 @@
 package code_block
 
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
 type Server struct {
 	Addr     string
 	Port     int
 	Protocol string
-	Timeout  int
+	Timeout  time.Duration
 	MaxConns int
-	TLS      string
+	TLS      *tls.Config
 }
 
+// Option 定义一个 func Option 来构建参数
 type Option func(*Server)
+
+func Protocol(p string) Option {
+	return func(s *Server) {
+		s.Protocol = p
+	}
+}
+func Timeout(timeout time.Duration) Option {
+	return func(s *Server) {
+		s.Timeout = timeout
+	}
+}
+func MaxConns(maxconns int) Option {
+	return func(s *Server) {
+		s.MaxConns = maxconns
+	}
+}
+func TLS(tls *tls.Config) Option {
+	return func(s *Server) {
+		s.TLS = tls
+	}
+}
+
+// NewServer 通过控制 Option 传入让 Server 的实例化高度可配置化
+// 相较于 builder 模式也不需要单独引入一个 builder 来控制
+func NewServer(addr string, port int, options ...func(*Server)) (*Server, error) {
+	srv := Server{
+		Addr:     addr,
+		Port:     port,
+		Protocol: "tcp",
+		Timeout:  30 * time.Second,
+		MaxConns: 1000,
+		TLS:      nil,
+	}
+	for _, option := range options {
+		option(&srv)
+	}
+	//...
+	return &srv, nil
+}
+
+func UseOptions() {
+	s1, _ := NewServer("localhost", 1024)
+	s2, _ := NewServer("localhost", 2048, Protocol("udp"))
+	s3, _ := NewServer("0.0.0.0", 8080, Timeout(300*time.Second), MaxConns(1000))
+	fmt.Println(s1, s2, s3)
+}
